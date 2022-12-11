5a79d02d2397f872f997ff4450266f3d3377c0e9
Jakub Kopecký <88899719+JacobGitH@users.noreply.github.com>
2022-12-11 16:49:18 +0100

    1-1

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace test // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] calories = System.IO.File.ReadAllLines(@"C:\programming\csharp\test\test\elfs.txt");
+            int maxCalories = 0;
+            int pom = 0;
+            foreach (string calory in calories)
+            {
+                if(string.IsNullOrEmpty(calory))
+                {
+                    if(maxCalories < pom)
+                    {
+                        maxCalories = pom;
+                    }
+                    pom = 0;
+                }
+                else
+                {
+                    pom += int.Parse(calory);
+                }
+            }
+            if(maxCalories < pom)
+            {
+                maxCalories = pom;
+            }
+            Console.WriteLine(maxCalories);
+        }
+    }
+}
